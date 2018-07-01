5d7847e85c005f7855326ec68cfb356d9f217446
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-01 17:51:22 -0600

    Data Models

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,35 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var btnNext: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        player = Player()
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "Mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "Womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "Coed")
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
-        performSegue(withIdentifier: "skillVCSeague", sender: <#T##Any?#>)
+        performSegue(withIdentifier: "skillVCSeague", sender: self)
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        btnNext.isEnabled = true
     }
     
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/1/18.
+//  Copyright © 2018 soy-raul. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
